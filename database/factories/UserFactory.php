f5cb0fb9d287844277c3f0185afb4019aece328a
damir <delewev.damir@gmail.com>
2022-09-06 08:35:03 +0300

    'route'

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Support\Str;
 use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,7 +14,7 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(App\User::class, function (Faker $faker) {
+$factory->define(\App\Models\User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
